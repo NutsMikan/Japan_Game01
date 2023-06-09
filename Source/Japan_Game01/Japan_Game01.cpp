2f11329fd0840482067beae9474371d6ff3f8b2b
amaken0226 <123623307+amaken0226@users.noreply.github.com>
2023-06-09 13:06:22 +0900

    Create Japan_Game01.cpp

---
+++ Source/Japan_Game01/Japan_Game01.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Japan_Game01.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Japan_Game01, "Japan_Game01" );
